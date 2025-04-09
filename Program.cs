1500bbd42073cfba88965e374e279865db53e1e5
alyaa999 <alyaamamoon999@gmail.com>
2025-04-10 00:14:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using e_commerce.Infrastructure.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_commerce
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ECommerceDBContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            //builder.Services.AddApplicationServices();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
